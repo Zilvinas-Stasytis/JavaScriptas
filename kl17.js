e763beb5f4f29975539e31d17b136db6cb8c42c3
Zilvinas <zalciukas@gmail.com>
2022-08-04 20:07:34 +0300

    s

--- kl17.js
+++ kl17.js
@@ -1,29 +1,35 @@
-
+alert("Pradedame užduotį");
 function createSchedule() {
-  let schedule = document.createElement("ul");
-  schedule.id = "schedule";
+  const schedule = document.createElement("ul");
+  schedule.className = "Schedule";
   schedule.style.width = "500px";
   schedule.style.height = "400px";
   schedule.style.backgroundColor = "aqua";
   schedule.style.marginLeft = "100px";
   schedule.style.display = "flex";
   schedule.style.flexDirection = "column";
-  schedule.style.justifyContent = "space-arround";
+  schedule.style.justifyContent = "flex-start";
   schedule.style.rowGap = "5px";
-  schedule.style.marginTop.firstChild = "30px";
+  schedule.style.textAlign = "center";
+  schedule.textContent = "Dienotvarkė " + Date();
   document.body.append(schedule);
 }
-document.addEventListener("click", createTasks);
+document.getElementById("createTask").addEventListener("click", createTasks);
 function createTasks(event) {
   event.preventDefault();
-  let task = document.createElement("li");
+  const taskText = document.getElementById("tache").value;
+  const task = document.createElement("li");
+  let mark = document.createElement("input");
   task.id = "task";
+  task.style.width = "500px";
+  task.style.height = "30px";
   task.style.listStyle = "none";
   task.style.marginLeft = "-40px";
   task.style.backgroundColor = "rgb(247, 0, 255)";
   task.style.textAlign = "left";
-  task.style.width = "500px";
-  task.style.height = "30px";
-  task.textContent = form.input.text;
+  task.textContent = taskText;
+  mark.type = "checkbox";
+  task.insertBefore(mark);
   schedule.appendChild(task);
+
 }
---
+++ kl20.js
@@ -0,0 +1,5 @@
+const getTodoItemsFromServer = () =>{
+    return new Promise(resolve  => {
+setTimeout()
+    }
+}
